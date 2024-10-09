d0fc08088259481420b85c5cd492e0cffedac40c
shengaojie <25591998145@qq.com>
2024-10-09 08:32:35 +0800

    将Executor01的test方法移动到Executor 02，并且删除Executor01

--- src/main/java/StructuredConcurency/executor_service/Executor02.java
+++ src/main/java/StructuredConcurency/executor_service/Executor02.java
@@ -1,21 +1,20 @@
 package StructuredConcurency.executor_service;
 
-
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
 /**
- * @Author shengaojie @Date 2023/9/7 14:35 @ClassName: executor01 @Description: TODO @Version 1.0
- */
-public class executor01 {
+ * @description:
+ * @author: shengaojie
+ * @create: 2024-10-09
+ **/
 
-    ExecutorService executor = Executors.newFixedThreadPool(4);
-
+public class Executor02 {
     @Test
     public void test() throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
         try {
             for (int i = 0; i < 4; i++) {
                 executor.submit(
